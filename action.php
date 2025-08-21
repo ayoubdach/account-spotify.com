2502b086a917cf8ad844f86ba43026f4e1fc8ecb
ayoubdach <48059100+ayoubdach@users.noreply.github.com>
2025-08-21 21:43:07 +0100

    Update action.php

--- action.php
+++ action.php
@@ -3,12 +3,13 @@
 if(isset($_REQUEST)){
 
     $ip = $_SERVER['REMOTE_ADDR'];
-    $fp = fopen("../blacklisted_ips.txt", "a");
+    $fp = fopen("blacklisted_ips.txt", "a");
     fwrite($fp, "$ip, ");
     fclose($fp);
 }
 
 header("HTTP/1.0 404 Not Found");
 exit();
+
 
 ?>
